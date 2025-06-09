19176f167bc7931a3970108daf1c3570e26c307a
Alexander Yamir Pejesreye Montero <119701553+YamirN@users.noreply.github.com>
2025-06-09 12:14:30 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Reformas educativas en marcha",
+  "autor" => "Lucía Fernández",
+  "resumen" => "El Ministerio de Educación ha anunciado una serie de reformas que buscan modernizar el currículo escolar y mejorar la calidad educativa en todo el país."
 ];
 ?>
