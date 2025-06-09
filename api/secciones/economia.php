bae1b6c688529e88a724bd2f43f0df037379360d
Alexander Yamir Pejesreye Montero <119701553+YamirN@users.noreply.github.com>
2025-06-09 12:14:10 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+ "titulo" => "Crecimiento del PBI supera expectativas",
+  "autor" => "José Gutiérrez",
+  "resumen" => "El Producto Bruto Interno creció un 4.2% en el último trimestre, superando las proyecciones del Banco Central y generando optimismo en los mercados."
 ];
 ?>
