fd73f23ebfbfe5b6abf3fa2de8c3702a36cfaba1
Alexander Yamir Pejesreye Montero <119701553+YamirN@users.noreply.github.com>
2025-06-09 12:14:41 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+  "titulo" => "Elecciones en Europa",
+  "autor" => "Carlos Ramírez",
+  "resumen" => "Los países europeos se preparan para una nueva jornada electoral que definirá..."
 ];
 ?>
